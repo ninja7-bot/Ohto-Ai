ee7a227a78262448e23839f5b302022ee2c91246
Dark Samurai <73326139+ninja7-bot@users.noreply.github.com>
2021-06-26 19:58:25 +0530

    Update youtube.py

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -7,6 +7,7 @@ from helpers.errors import DurationLimitError
 
 ydl_opts = {
     "format": "bestaudio[ext=m4a]",
+    "addmetadata": True,
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
